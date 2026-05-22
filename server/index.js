f36334a43de48d4ecf8d47c0168a20916875e0af
Shiva854431 <shivanakkanagoni17@gmail.com>
2026-05-22 19:07:24 +0530

    Fix wildcard route error for Express 5.x

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json())
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'dist')))
-  app.get('*', (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'))
   })
 }
