5c25528648ebbd04a7fb7543a8e1742e305201d9
Shiva854431 <shivanakkanagoni17@gmail.com>
2026-05-23 07:55:46 +0530

    Add timeout to nodemailer to prevent hanging on email send

--- server/mail.js
+++ server/mail.js
@@ -7,6 +7,9 @@ export function createTransporter() {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   })
 }
 
